da3bef99cf11a528d3420d46834e5bc837237168
Srikanth Nagella <srikanth.nagella@stfc.ac.uk>
2018-09-15 02:25:59 +0100

    updated release version

--- setup.py
+++ setup.py
@@ -169,7 +169,7 @@ extensions = [
     ]    
 setup(
      name = 'survos',
-	 version = "1.1.2",
+	 version = "1.1.3",
 	 description = '(Su)per (R)egion (Vo)lume (S)egmentaton workbench',
 	 license='GPL',
      packages = find_packages(),
